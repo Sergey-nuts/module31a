e27ce0efda58d067f053a8d8c60ca0638d95762a
Sergey-nuts <DeathlessNuts@mail.ru>
2023-06-03 00:42:00 +0300

    add ERD.png and schema.sql

---
+++ schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS authors, posts;
+
+CREATE TABLE authors (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+    id BIGSERIAL PRIMARY KEY,
+    author_id BIGINT REFERENCES authors(id) DEFAULT 0,
+    title TEXT NOT NULL,
+    content TEXT,
+    created_at BIGINT NOT NULL DEFAULT extract(epoch from now())
+);
+
+INSERT INTO authors(id, name) VALUES(0, 'default');
